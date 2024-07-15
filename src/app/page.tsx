e67e0dd0622330ec895f3751f87c1906fa1219b8
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-07-16 02:36:44 +0900

    refactor: Simplify song request form in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,9 @@ import requestSongAction from "@/action/requestSong";
 import SongList from "@/components/list";
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
-import prisma from "@/lib/prisma";
 import {cn} from "@/lib/utils";
 
 export default async function Home() {
-  const session = (await prisma.session.findMany())[0];
-
   return (
     <main className="grid min-h-dvh grid-cols-1 grid-rows-[1fr,auto]">
       <section
@@ -19,21 +16,13 @@ export default async function Home() {
         <form
           action={requestSongAction}
           className="flex flex-col lg:gap-4 gap-2 w-full">
-          <Input
-            name="song-name"
-            placeholder={
-              session ? "ชื่อเพลง ..." : "ไม่สามารถขอเพลงได้ในตอนนี้"
-            }
-            disabled={!session}
-          />
-          <Button type="submit" disabled={!session}>
-            ขอเพลง
-          </Button>
+          <Input name="song-name" placeholder={"ชื่อเพลง ..."} />
+          <Button type="submit">ขอเพลง</Button>
         </form>
       </section>
       <section
         className={cn("flex justify-center items-center", "lg:p-12", "p-4")}>
-        {!session ? "ไม่สามารถขอเพลงได้ในตอนนี้" : <SongList />}
+        <SongList />
       </section>
     </main>
   );
