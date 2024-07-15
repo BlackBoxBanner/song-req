8851d0c3e9a0d60fc8507d5a698b24929b01da16
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-07-16 02:39:33 +0900

    refactor: Simplify song request form in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,13 @@ export default async function Home() {
   return (
     <main className="grid min-h-dvh grid-cols-1 grid-rows-[1fr,auto]">
       <section
-        className={cn(
-          "flex justify-center items-center",
-          "lg:p-12 lg:order-first",
-          "p-4 order-last"
-        )}>
-        <form
-          action={requestSongAction}
-          className="flex flex-col lg:gap-4 gap-2 w-full">
+        className={cn("flex justify-center items-center", "p-4 order-last")}>
+        <form action={requestSongAction} className="flex flex-col gap-2 w-full">
           <Input name="song-name" placeholder={"ชื่อเพลง ..."} />
           <Button type="submit">ขอเพลง</Button>
         </form>
       </section>
-      <section
-        className={cn("flex justify-center items-center", "lg:p-12", "p-4")}>
+      <section className={cn("flex justify-center items-center", "p-4")}>
         <SongList />
       </section>
     </main>
