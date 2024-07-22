8dc7e091acf03ed3005529f0cdb1933be6fa984e
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-07-22 22:07:32 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default async function Home() {
     <main className="grid min-h-dvh grid-cols-1 grid-rows-[auto,1fr,auto] gap-6 p-4">
       {session ? (
         <>
+          <InfiniteScroll />
           <section className={cn("flex justify-center items-center")}>
             <SongList initData={song} />
           </section>
@@ -27,3 +28,36 @@ export default async function Home() {
     </main>
   );
 }
+
+const InfiniteScroll = () => {
+  return (
+    <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+      <ul className="text-nowrap flex items-center justify-center md:justify-start [&_li]:mx-4 [&_img]:max-w-none animate-infinite-scroll">
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+      </ul>
+      <ul
+        className="text-nowrap flex items-center justify-center md:justify-start [&_li]:mx-4 [&_img]:max-w-none animate-infinite-scroll"
+        aria-hidden="true">
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+        <li>พื้นที่ขอเพลง</li>
+      </ul>
+    </div>
+  );
+};
