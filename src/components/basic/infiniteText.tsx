fc8764a51829da3a80723e6fd8fee06e12902915
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-07-31 15:27:13 +0900

    refactor: Improve infinite scrolling behavior in InfiniteText component

--- src/components/basic/infiniteText.tsx
+++ src/components/basic/infiniteText.tsx
@@ -1,8 +1,10 @@
 const InfiniteText = ({text = "ตัวอย่าง"}: {text: string}) => {
-  const items = Array(10).fill(text);
+  const items = Array(15).fill(text);
 
   return (
-    <div className="w-full inline-flex flex-nowrap overflow-hidden">
+    <div className="w-full inline-flex flex-nowrap overflow-hidden relative">
+      <span className="bg-gradient-to-r from-background via-background/90 to-transparent absolute top-0 left-0  h-full w-[6rem] z-10" />
+      <span className="bg-gradient-to-l from-background via-background/90 to-transparent absolute top-0 right-0  h-full w-[6rem] z-10" />
       {["", ' aria-hidden="true"'].map((aria, index) => (
         <ul
           key={index}
