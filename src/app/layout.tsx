eea070a5c872a90c1e5d058e3f676289992beaad
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-09-18 20:43:52 +0700

    refactor: Clear all code for new start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import type {Metadata} from "next";
-import {Maitree} from "next/font/google";
+import type { Metadata } from "next";
+import { Maitree } from "next/font/google";
 import "./globals.css";
-import {Toaster} from "@/components/ui/toaster";
-import {SocketProvider} from "@/components/context/socketContext";
-import {SessionProvider} from "@/components/context/sessionContext";
+import { Toaster } from "@/components/ui/toaster";
+import { SocketProvider } from "@/components/context/socketContext";
 import InitSocket from "@/components/client/initSocket";
 
 const maitree = Maitree({
@@ -15,21 +14,23 @@ export const metadata: Metadata = {
   title: "ขอเพลง",
   description: "แอปพลิเคชันสำหรับขอเพลงขณะไลฟ์สด",
   keywords: ["ขอเพลง", "ไลฟ์สด", "เพลง", "แอปพลิเคชัน"],
-  authors: [{name: "BlackBoxBanner"}],
+  authors: [{ name: "BlackBoxBanner" }],
   creator: "BlackBoxBanner",
   applicationName: "ขอเพลง",
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="th">
       <body className={maitree.className}>
         <Toaster />
         <SocketProvider>
-          <SessionProvider>
-            <InitSocket />
-            {children}
-          </SessionProvider>
+          <InitSocket />
+          {children}
         </SocketProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,6 @@
-import InfiniteText from "@/components/basic/infiniteText";
-import RequestSongInputForm from "@/components/client/reqInput";
-import SongList from "@/components/list";
 import prisma from "@/lib/prisma";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
 export default async function Home() {
-  const songs = await prisma.song.findMany({
-    orderBy: {
-      createAt: "asc",
-    },
-  });
-
-  return (
-    <main className="snap-mandatory snap-y h-dvh">
-      <section className="grid min-h-dvh grid-cols-1 grid-rows-[auto,1fr,auto] snap-start">
-        <div className="sticky top-0 bg-background z-10 py-4 border-b">
-          <InfiniteText text="พื้นที่ขอเพลง" />
-        </div>
-        <section
-          className={cn("flex justify-center items-center overflow-clip px-0")}>
-          <SongList initData={songs} />
-        </section>
-        <RequestSongInputForm />
-      </section>
-      {/* Additional sections can be added here */}
-    </main>
-  );
+  return <main className="snap-mandatory snap-y h-dvh"></main>;
 }
