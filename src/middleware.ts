4906c3dacc494970738e1124964a004e02452ba5
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-09-21 15:40:17 +0700

    Refactor middleware to handle authentication and routing logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,26 @@
-export { auth as middleware } from "@/lib/auth";
+import authConfig from "@/config/auth.config";
+import NextAuth from "next-auth";
+import { NextResponse } from "next/server";
+
+const { auth } = NextAuth(authConfig);
+export default auth(async (req) => {
+  const isLoggedIn = !!req.auth;
+  const isOnCreator = req.nextUrl.pathname.startsWith("/creator");
+  const isOnAuth = req.nextUrl.pathname.startsWith("/auth");
+
+  if (isOnCreator) {
+    if (isLoggedIn) return NextResponse.next();
+    return NextResponse.rewrite(new URL("/auth", req.nextUrl));
+  }
+
+  if (isOnAuth) {
+    if (isLoggedIn)
+      return NextResponse.redirect(new URL("/creator", req.nextUrl));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
