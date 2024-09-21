b9ccb05b0f08afa4c400ab64dfac162a9c7c1fea
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-09-21 15:39:12 +0700

    Add login page component

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,44 @@
+import SignInForm from "@/components/client/signinForm";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const LoginPage = () => {
+  return (
+    <main
+      className={cn("bg-background h-dvh flex justify-center md:items-center")}
+    >
+      <Card className="w-full max-w-96 m-2">
+        <CardHeader>
+          <CardTitle>Auth</CardTitle>
+          <CardDescription>
+            Please sign in to your account or register to create a new one.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2">
+            <Link href={"/auth/signin"}>
+              <Button className="w-full" variant={"outline"}>
+                Sign in
+              </Button>
+            </Link>
+            <Link href={"/auth/register"}>
+              <Button className="w-full" variant={"outline"}>
+                Sign up
+              </Button>
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
+  );
+};
+
+export default LoginPage;
