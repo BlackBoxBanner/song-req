489a80df2b3e72b1658acbb17a45724e771558fd
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-10-19 21:15:40 +0700

    Refactor auth page layout and styles

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -13,30 +13,32 @@ import Link from "next/link";
 const LoginPage = () => {
   return (
     <main
-      className={cn("bg-background h-dvh flex justify-center md:items-center")}
+      className={cn("bg-background h-dvh flex justify-center items-center")}
     >
-      <Card className="w-full max-w-96 m-2">
-        <CardHeader>
-          <CardTitle>Auth</CardTitle>
-          <CardDescription>
-            Please sign in to your account or register to create a new one.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col gap-2">
-            <Link href={"/auth/signin"}>
-              <Button className="w-full" variant={"outline"}>
-                Sign in
-              </Button>
-            </Link>
-            <Link href={"/auth/register"}>
-              <Button className="w-full" variant={"outline"}>
-                Sign up
-              </Button>
-            </Link>
-          </div>
-        </CardContent>
-      </Card>
+      <div>
+        <Card className="w-full max-w-96 m-2">
+          <CardHeader>
+            <CardTitle>Auth</CardTitle>
+            <CardDescription>
+              Please sign in to your account or register to create a new one.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-2">
+              <Link href={"/auth/signin"}>
+                <Button className="w-full" variant={"outline"}>
+                  Sign in
+                </Button>
+              </Link>
+              <Link href={"/auth/register"}>
+                <Button className="w-full" variant={"outline"}>
+                  Sign up
+                </Button>
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 };
