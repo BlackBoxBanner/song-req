e420d679fa684b3a0c9c79439a154d87821bf560
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-10-19 21:16:23 +0700

    Refactor auth page layout and styles

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -15,30 +15,28 @@ const LoginPage = () => {
     <main
       className={cn("bg-background h-dvh flex justify-center items-center")}
     >
-      <div>
-        <Card className="w-full max-w-96 m-2">
-          <CardHeader>
-            <CardTitle>Auth</CardTitle>
-            <CardDescription>
-              Please sign in to your account or register to create a new one.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex flex-col gap-2">
-              <Link href={"/auth/signin"}>
-                <Button className="w-full" variant={"outline"}>
-                  Sign in
-                </Button>
-              </Link>
-              <Link href={"/auth/register"}>
-                <Button className="w-full" variant={"outline"}>
-                  Sign up
-                </Button>
-              </Link>
-            </div>
-          </CardContent>
-        </Card>
-      </div>
+      <Card className="w-full max-w-96 m-2">
+        <CardHeader>
+          <CardTitle>Auth</CardTitle>
+          <CardDescription>
+            Please sign in to your account or register to create a new one.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2">
+            <Link href={"/auth/signin"}>
+              <Button className="w-full" variant={"outline"}>
+                Sign in
+              </Button>
+            </Link>
+            <Link href={"/auth/register"}>
+              <Button className="w-full" variant={"outline"}>
+                Sign up
+              </Button>
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
     </main>
   );
 };
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 const LoginPage = () => {
   return (
     <main
-      className={cn("bg-background h-dvh flex justify-center md:items-center")}
+      className={cn("bg-background h-dvh flex justify-center items-center")}
     >
       <Card className="w-full max-w-96 m-2">
         <CardHeader>
